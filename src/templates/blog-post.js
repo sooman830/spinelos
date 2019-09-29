1460cb79332504a554f0469edffe3dad291d521e
Soma Nakamura <47101025+sooman830@users.noreply.github.com>
2019-09-29 16:27:35 +0900

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -100,20 +100,3 @@ export const pageQuery = graphql`
       }
     }
   }
-  
-  const Meta = ({ post }) => {
-  const origin = 'https://shibe97.com';
-
-  return (
-    <Helmet
-      title={`${post.frontmatter.title} | Blog`}
-      meta={[
-        { name: 'description', content: post.frontmatter.description },
-        { property: 'og:title', content: post.frontmatter.title },
-        { property: 'og:description', content: post.frontmatter.description },
-        { property: 'og:image', content: `${origin}${post.frontmatter.image}` },
-      ]}
-    />
-  );
-};
-`
